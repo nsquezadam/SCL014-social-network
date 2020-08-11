5e601bbdefef288d88ba4daf8afe132320de9117
JenifferGenoves <62884668+JenifferGenoves@users.noreply.github.com>
2020-08-11 18:47:12 -0400

    Revert "Historia usuario 1"

--- src/main.js
+++ src/main.js
@@ -1,17 +1,5 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
-// import { logIn } from './lib/controllers/firebasecontroller.js';
-import { welcome } from './lib/view/WelcomeTemplate.js';
-import { changeRoute } from './lib/router.js';
 
-const init = () => {
- document.getElementById('root').appendChild(welcome());
-  window.addEventListener('hashchange', () => {
-    myFunction();
-    // eslint-disable-next-line no-console
-    console.log(window.location.hash);
-    changeRoute(window.location.hash);
-  });
-};
-window.addEventListener('load', init);
+myFunction();
